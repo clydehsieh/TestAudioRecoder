43b8c36bab2c3f92962b85c4850fb3d4dd903a96
clydehsieh <clydehsieh0601@gmail.com>
2015-05-13 16:20:01 +0800

    Initial Commit

---
+++ TestRecordVoice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestRecordVoice
+//
+//  Created by Chin-Hui Hsieh  on 5/13/15.
+//  Copyright (c) 2015 Chin-Hui Hsieh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
